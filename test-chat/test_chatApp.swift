5f7e188d6c799c5a730daab4a197d1e83ecceff8
nurkholis <nur.kholis@ist.id>
2022-11-23 16:50:05 +0700

    Initial Commit

---
+++ test-chat/test_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_chatApp.swift
+//  test-chat
+//
+//  Created by nur kholis on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct test_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test-chatUITests/test_chatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_chatUITestsLaunchTests.swift
+//  test-chatUITests
+//
+//  Created by nur kholis on 23/11/22.
+//
+
+import XCTest
+
+final class test_chatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

999ecda4b8a74a23e76beeaf1a4014cac5343b01
nurkholis <nur.kholis@ist.id>
2022-11-24 10:35:32 +0700

    as

--- test-chat/ContentView.swift
+++ test-chat/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var fruits = ["Anggur", "Apel", "jeruk"]
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                List(0..<3) { item in
+                    SectionFruitView(fruits: fruits)
+                }.listStyle(.sidebar)
+                    .foregroundColor(.accentColor)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    var item: String
+    var body: some View {
+        Text(item)
+    }
+}
+
+struct SectionFruitView: View {
+    var fruits: [String]
+    
+    var body: some View {
+        Section {
+            ForEach(fruits, id: \.self) { fruit in
+                Label(fruit, systemImage: "\(fruits.firstIndex(of: fruit) ?? 0).circle.fill" )
+            }
+        } header: {
+            ExtractedView(item: "Fruits")
+        }
     }
 }
